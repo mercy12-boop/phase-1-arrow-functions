deb24f5ce853084ef3c8ab0d24fc830185884278
Mercy Wanjiru <wanjirumercy54@gmail.com>
2024-03-27 17:28:09 +0300

    Completes Lab

---
+++ index.js
@@ -0,0 +1,15 @@
+function divide(a, b) {
+  return a / b;
+}
+// const sum = (parameter1, parameter2) => {
+//   console.log(`Adding ${parameter1}`);
+//   console.log(`Adding ${parameter2}`);
+//   return parameter1 + parameter2;
+// }
+// sum(1,2); //=> 3
+
+// const twoAdder = x => x + 2;
+// // is the same as
+// const twoAdder = (x) => x + 2;
+const square = (x) => x * x;
+const add = (x, y) => x + y;
